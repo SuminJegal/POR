94cd2bac00fde47b69fc33ad27359f780bd0e88c
LeeKwangWoong <38875271+LeeKwangWoong@users.noreply.github.com>
2018-05-09 20:30:05 +0900

    Add Result model with dummy data

--- webui/views.py
+++ webui/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from. models import Result
 
-# Create your views here.
 def home(request):
     search = ""
+    results = Result.dummy
+
     if (request.method == 'POST'):
         search = request.POST['search']
-    return render(request, 'webui/home.html', {'search':search})
 
+    return render(request, 'webui/home.html', {'search':search, 'results':results})
