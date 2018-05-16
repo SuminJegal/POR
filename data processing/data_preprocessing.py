b5da92612b2b8156dc3005513e848c15976d15ab
SuminJegal <geniusry@naver.com>
2018-05-16 14:02:51 +0900

    Upload preprocessing code

--- data processing/data_preprocessing.py
+++ data processing/data_preprocessing.py
@@ -2,6 +2,7 @@ import io
 import re
 from konlpy.tag import Twitter
 import codecs
+import csv
 
 # "."을 기준으로 문장을 나누고, 영어, 특수문자 등을 지움
 def data_preprocessing():
@@ -19,23 +20,39 @@ def data_preprocessing():
             fw.writelines(item)
 
 # 명사만 추출
-def extract_noun():
+def extract_phrases_txt():
     twitter = Twitter()
     read_file = io.open('data_cleansed.txt', 'r', encoding='utf-8')
     write_file = codecs.open('phrases.txt', 'w', encoding='utf-8')
     for line in read_file:
-        item = ' '.join(("{}".format(word)) for word in twitter.phrases(line)) + '\n'
+        item = ' '.join(("{}".format(word)) for word in twitter.phrases(line))
         write_file.write(item)
         # print(item)
     read_file.close()
     write_file.close()
 
 
+#구로 추출 csv로 저장
+def extract_phrases_csv():
+    print("\nNow extract phrases ... ")
+    data = io.open('data_cleansed.txt', 'r', encoding='utf-8')
+    words = list()
+    tw = Twitter()
+    for item in data:
+        words = words + tw.phrases(item)
+    print("\nNow save as csv file ... ")
+    csvfile = 'phrases.csv'
+    with open(csvfile, 'w', encoding='utf-8') as output:
+        writer = csv.writer(output, lineterminator='\n')
+        for val in words:
+            writer.writerow([val])
+
+
+
 def main():
-    print("Data preprocessing ... ")
-    data_preprocessing()
-    print("\n \n \n \nNow extract phrases ... ")
-    extract_noun()
+    # print("Data preprocessing ... ")
+    # data_preprocessing()
+    extract_phrases_csv()
 
 if __name__ == '__main__':
     main()
